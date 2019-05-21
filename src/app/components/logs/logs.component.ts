04c74cb428ca96fe8b23d478da8e8b7d01588250
gpequery <g.pequery@gmail.com>
2019-05-21 14:22:24 +0200

    create logs components & refacto

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.scss']
 })
-export class LogsComponent implements OnInit {
+export class LogsComponent {
+  logs: string = "Match Start !";
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
