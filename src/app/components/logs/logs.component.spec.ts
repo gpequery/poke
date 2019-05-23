430fc4a512bf1db33fffeccb04c24c6271f620c1
rgoudard <remrem123>
2019-05-23 20:00:52 +0200

    Add test for logs component

--- src/app/components/logs/logs.component.spec.ts
+++ src/app/components/logs/logs.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogsComponent } from './logs.component';
+import {Family, Logs, Pokemon} from '../../models';
+import {PCustomColor} from '../../directives';
+import {By} from '@angular/platform-browser';
 
 describe('LogsComponent', () => {
   let component: LogsComponent;
@@ -8,7 +11,7 @@ describe('LogsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LogsComponent ]
+      declarations: [ LogsComponent , PCustomColor ]
     })
     .compileComponents();
   }));
@@ -16,10 +19,31 @@ describe('LogsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LogsComponent);
     component = fixture.componentInstance;
+    let pokemon = new Pokemon('myName1', 50, new Family('fire', 'success'), null);
+    let attackData = [50, 2];
+
+
+    component.logs = [ new Logs(pokemon , 'oui', 'NON', attackData , false)
+    ];
+
     fixture.detectChanges();
+  });
+
+  it('should instantiate', () => {
+    expect(component).toBeDefined();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('logs with 1 line are created ', () => {
+    const divElement = fixture.debugElement.query(By.css('.logs'));
+    let logLine = divElement.nativeElement.textContent.trim();
+    let namePkm = component.logs[0].pokemon.name;
+    let nameAttack = component.logs[0].attackLabel;
+    let damage = component.logs[0].attackData[0];
+    let modifier = component.logs[0].attackData[1];
+    expect(logLine).toContain(namePkm && nameAttack && damage && modifier);
   });
 });
